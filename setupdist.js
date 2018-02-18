7f240950d307dc6376b81a1124d0d2aa8a74ab27
David Waller <david@krawaller.se>
2018-02-18 22:08:39 +0100

    rename index template

--- setupdist.js
+++ setupdist.js
@@ -1,4 +1,4 @@
-// script for setting up the dist folder for the React app,
+// script for setting up the dist folder
 // before webpack adds bundle.js
 
 var fs = require("fs-extra");
@@ -6,6 +6,6 @@ var join = require("path").join;
 
 fs.emptyDirSync(join(__dirname, "dist"));
 
-fs.copySync(join(__dirname, "src/appdoc.html"), join(__dirname, "dist/index.html"));
+fs.copySync(join(__dirname, "appindex.html"), join(__dirname, "dist/index.html"));
 
 fs.copySync(join(__dirname, "node_modules/todomvc-app-css/index.css"), join(__dirname, "/dist/todomvc.css"));
